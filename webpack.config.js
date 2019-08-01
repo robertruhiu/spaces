e49463b6fdde3e6821173b89d7ba98c6f9e4fecb
frenx <robertruhiu@gmail.com>
2019-08-01 14:09:27 +0000

    split chunking on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,27 @@ module.exports = {
     mode: 'development',
 
     optimization: {
-        splitChunks: {
-            chunks: 'all'
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      automaticNameMaxLength: 30,
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
         }
+      }
     }
+  }
 };
