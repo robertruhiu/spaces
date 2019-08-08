3f141d680a2d6c9e10923aae29c7c58e14246437
frenx <robertruhiu@gmail.com>
2019-08-08 21:13:03 +0000

    specified node and npm version

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+
 // const serveStatic = require('serve-static');
 let expressStaticGzip = require("express-static-gzip");
 let app = express();
