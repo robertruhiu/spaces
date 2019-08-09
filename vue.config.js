e7ad9960381bd9dbf6e8a9314cb86231bb096304
frenx <robertruhiu@gmail.com>
2019-08-09 00:38:43 +0000

    restored brotli compression

--- vue.config.js
+++ vue.config.js
@@ -5,12 +5,12 @@ module.exports = {
             hints: false
         },
         plugins: [
-            // new BrotliPlugin({
-            //     asset: '[path].br[query]',
-            //     test: /\.js$|\.css$|\.html$/,
-            //     threshold: 10240,
-            //     minRatio: 0.7
-            // }),
+            new BrotliPlugin({
+                asset: '[path].br[query]',
+                test: /\.js$|\.css$|\.html$/,
+                threshold: 10240,
+                minRatio: 0.7
+            }),
 
 
         ],
