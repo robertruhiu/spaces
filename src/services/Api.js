db9c6500b0992dfd18dadf203c9b538eac5472d3
Philisiah <sphilisiah@gmail.com>
2019-08-13 08:24:12 +0000

    edit: api

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default () => {
   return axios.create({
     // baseURL: process.env.VUE_APP_PATH
-    // baseURL: `https://codelnapi.herokuapp.com`
-    baseURL: `http://127.0.0.1:8000`
+    baseURL: `https://codelnapi.herokuapp.com`
+    // baseURL: `http://127.0.0.1:8000`
   })
 }
